a6fead1fe812b9da41996b826bb97d31e5c30fc0
Brahmmak507 <65611216+Brahmmak507@users.noreply.github.com>
2024-12-05 16:18:52 +0530

    Update CreateAuthCookie.cs

--- IndividualChallenges/CreateAuthCookie.cs
+++ IndividualChallenges/CreateAuthCookie.cs
@@ -1,11 +1,15 @@
 public void CreateAuthCookie(string userInput)
 {
     // Creating a new cookie with the user's input
-    HttpCookie authCookie = new HttpCookie("auth", userInput)
+    try{
+        HttpCookie authCookie = new HttpCookie("auth", userInput)
     {
         HttpOnly = true,
         Secure = true
     };
     HttpContext.Current.Response.Cookies.Add(authCookie);
     Console.WriteLine("Cookie has been set.");
+ catch (Exception ex){
+     Console.WriteLine("An error occurred while setting the cookie.");
+ }
 }
